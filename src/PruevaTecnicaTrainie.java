33b02e3435e2b6de06ce3c67ba3871575a58dc45
brauliogm <brauliogm16@gmail.com>
2023-10-11 12:57:29 -0600

    Agregando metodo de imprecion

--- src/PruevaTecnicaTrainie.java
+++ src/PruevaTecnicaTrainie.java
@@ -3,25 +3,30 @@
 public class PruevaTecnicaTrainie {
     public static void main(String[] args) {
         String[][] teatro = new String[10][10];
-        teatro[0][0] = "X";
+        teatro[5][5] = "X";
 
+        impresionAsientos(teatro);
+
+    }
+
+    public static void impresionAsientos(String[][] teatro){
+        System.out.print("T ");
+        for (int k = 0; k < 10; k++) {
+            System.out.print(k + " ");
+        }
+
+        System.out.println(" ");
         for (int i = 0; i < teatro.length; i++) {
             System.out.print(0 + (i) +" ");
             for (int j = 0; j < teatro.length; j++) {
-                if (i == 0){
-                    System.out.print(j +" ");
-                    System.out.println(" ");
+                if (teatro[i][j] == null ){
+                    teatro[i][j] ="L";
                 }
-                teatro[i][j] = "O";
                 System.out.print(teatro[i][j] + " ");
                 if ( j == 9){
                     System.out.println(" ");
                 }
             }
         }
-
-
     }
-
-
 }

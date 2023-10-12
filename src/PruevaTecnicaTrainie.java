5091df3555285f525c4aee864426db998e3965d1
brauliogm <brauliogm16@gmail.com>
2023-10-12 13:14:36 -0600

    Metodo para escoger el asiento en el anfiteatro

--- src/PruevaTecnicaTrainie.java
+++ src/PruevaTecnicaTrainie.java
@@ -1,15 +1,44 @@
-
+import java.util.Scanner;
 
 public class PruevaTecnicaTrainie {
     public static void main(String[] args) {
         String[][] teatro = new String[10][10];
-        teatro[5][5] = "X";
+        Scanner sc = new Scanner(System.in);
+        boolean ocupado = true;
+
+        teatro[5][6] = "X";
+
+        impresionAsientos(teatro);
+
+        pedirAsiento(teatro, sc, ocupado);
 
         impresionAsientos(teatro);
 
     }
 
+    private static void pedirAsiento(String[][] teatro, Scanner sc, boolean ocupado) {
+        while (ocupado == true){
+            System.out.println("Escoja que asiento quiere indicando las coordenadas segun su");
+            try {
+            System.out.println("\"Fila\" (vertical)");
+            int fila = sc.nextInt();
+            System.out.println("\"Asiento\" (horizontal)");
+            int asiento = sc.nextInt();
+            if (teatro[fila][asiento] == "X"){
+                System.out.println("Asiento ocupado, escoja otro.");
+            }else{
+                teatro[fila][asiento] = "X";
+                ocupado = false;
+            }
+            } catch (Exception e){
+                System.out.println("Numero de asiento invalido");
+            }
+        }
+    }
+
     public static void impresionAsientos(String[][] teatro){
+        System.out.println("Asientos disponibles");
+        System.out.println(" *****ESCENARIO*****");
         System.out.print("T ");
         for (int k = 0; k < 10; k++) {
             System.out.print(k + " ");

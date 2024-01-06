eb373b59202a72755b18a4bfbba25583dce28b76
brauliogm <brauliogm16@gmail.com>
2024-01-06 15:19:42 -0600

    3 nuevos ejercicios

---
+++ src/codewars/DoubleChar.java
@@ -0,0 +1,16 @@
+package codewars;
+
+public class DoubleChar {
+    /*
+    Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+    Examples (Input -> Output):
+    * "String"      -> "SSttrriinngg"
+    * "Hello World" -> "HHeelllloo  WWoorrlldd"
+    * "1234!_ "     -> "11223344!!__  "
+     */
+
+    public static String doubleChar(String s){
+        //enter your code here
+    }
+}
---
+++ src/codewars/ExclamationMarks.java
@@ -0,0 +1,26 @@
+package codewars;
+
+public class ExclamationMarks {
+    /*
+    Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+    Examples
+    remove("Hi!",1) === "Hi"
+    remove("Hi!",100) === "Hi"
+    remove("Hi!!!",1) === "Hi!!"
+    remove("Hi!!!",100) === "Hi"
+    remove("!Hi",1) === "Hi"
+    remove("!Hi!",1) === "Hi!"
+    remove("!Hi!",100) === "Hi"
+    remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+    remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+    remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+    remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+     */
+
+    public static String remove(String s, int n){
+
+        return s;
+
+    }
+}
